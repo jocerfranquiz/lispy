b60bf61da6a768df3514b0012d4c80c58db0445a
Jocer <jocerfranquiz@gmail.com>
2023-07-10 00:59:58 -0500

    Update variables_test.py

--- tests/variables_test.py
+++ tests/variables_test.py
@@ -2,5 +2,5 @@ from . import *
 
 assert compute("[var x 7]") == 7
 assert compute("x") == 7
-assert compute("VERSION") == "lispy version 0.0.1 2023-07-06"
+assert compute("VERSION") == "tea version 0.0.1 2023-07-06"
 assert compute("[var is_user true]") is True
